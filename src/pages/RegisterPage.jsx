20a9662c9a84456f2ab40337793a52910e70dbd0
colyn0828 <colyngao@gmail.com>
2021-04-14 09:54:03 -0600

    update

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import {Redirect} from 'react-router-dom';
+import firebaseApp from '../firebase/firebaseConfig.js';
+
 import FormInput from './../components/forms/FormInput'
 import Button from './../components/buttons/Button'
 
@@ -19,18 +22,36 @@ const RegisterPageStyles = styled.aside `
 `
 
 const RegisterPage = (props) => {
-    return ( 
-        <RegisterPageStyles>
-        <header>
-            <h2>Unlimited Free Trial Sign Up</h2>
-            <p>no credit card required</p>
-        </header>
-        <FormInput label="name on account" type="text"/>
-        <FormInput label="valid email address" type="email"/>
-        <FormInput label="password (min 6 characters)" type="password"/>
-        <Button className = "create-accounts" label="create a free account" uiStyle="signup"/>
-        </RegisterPageStyles>
-     );
+    const [email, setEmail] = useState('');
+    const [password, setPassword] =  useState('');
+    const [hasFinished, setHasFinished] = useState(false);
+
+    function handleNewAccount (){
+        firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+        .then(userCredential=> {
+            setHasFinished(true);
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
+
+    if(hasFinished){
+        return <Redirect to="/login" />
+    } else {
+        return ( 
+            <RegisterPageStyles>
+            <header>
+                <h2>Unlimited Free Trial Sign Up</h2>
+                <p>no credit card required</p>
+            </header>
+            <FormInput label="name on account" type="text"/>
+            <FormInput label="valid email address" type="email" onChange = {(e)=> setEmail(e.target.value.trim())}/>
+            <FormInput label="password (min 6 characters)" type="password" onChange={(e)=>setPassword(e.target.value.trim())}/>
+            <Button className = "create-accounts" label="create a free account" uiStyle="signup" onClick = {handleNewAccount}/>
+            </RegisterPageStyles>
+         );
+    }   
 }
  
 export default RegisterPage;
